96ea83cb3d05f7f601f5faf258b80e82ee908726
kkota <kota111709@outlook.jp>
2020-05-07 00:36:58 +0900

    add map

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const btn = document.querySelector('button');
+const btn1 = document.querySelector('.juke');
+const btn2 = document.querySelector('.gps');
 
 const isSupported = !!(
     'ontouchstart' in window ||
@@ -17,7 +18,7 @@ const URLs = ['https://www.youtube.com/watch?v=ony539T074w',
     'https://www.youtube.com/watch?v=RLAw8Ct9k48',
     'https://www.youtube.com/watch?v=XDiuwqJg8pU'];
 
-btn.addEventListener('click', (event) => {
+btn1.addEventListener('click', (juke) => {
     const isYes = confirm(`${URLs.length}曲からランダムに選ばれます`);
 
     if(isYes){
@@ -26,3 +27,27 @@ btn.addEventListener('click', (event) => {
         win.focus();
     }
 });
+
+btn2.addEventListener('click', (gps) => {
+    const isYes = confirm('位置情報ください');
+
+    if(isYes){
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+});
+
+function geoSuccess(position){
+    const lat = position.coords.latitude;
+
+    const lng = position.coords.longitude;
+
+    const state = `https://fukuno.jig.jp/app/printmap/latlngmap.html#15/${lat}/${lng}/&base=std&ls=std&disp=1&vs=c1j0l0u0f1`;
+
+    const win = window.open(`${state}`);
+    win.focus();
+}
+
+
+function geoError(){
+    alert('Geolocation Error');
+}
