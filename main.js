9839c94c31688aa382004d705a77dbc6cd798ad0
kkota <kota111709@outlook.jp>
2020-05-06 19:38:18 +0900

    とりあえず見れる

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const btn = document.querySelector('button');
 
+const URLs = ['https://www.youtube.com/watch?v=ou_oYzY4hNk&feature=emb_rel_pause',
+    'https://www.youtube.com/watch?v=hOrTU2NNDVQ'];
+
 btn.addEventListener('click', (event) => {
     const isYes = confirm('random？');
 
     if(isYes){
-        console.log(location.href);
-        location.href = 'https://www.youtube.com/watch?v=ou_oYzY4hNk&feature=emb_rel_pause';
+        const random = Math.floor(Math.random()*URLs.length);
+        const win = window.open(`${URLs[random]}`);
+        win.focus();
     }
 });
