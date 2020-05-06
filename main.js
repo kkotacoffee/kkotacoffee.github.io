c4021895970ca83cd0ba9715546096ecc99e1ac6
kkota <kota111709@outlook.jp>
2020-05-07 01:09:19 +0900

    redo

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const btn1 = document.querySelector('.juke');
-const btn2 = document.querySelector('.gps');
 
 const isSupported = !!(
     'ontouchstart' in window ||
@@ -27,25 +26,3 @@ btn1.addEventListener('click', (juke) => {
         win.focus();
     }
 });
-
-btn2.addEventListener('click', (gps) => {
-    const isYes = confirm('位置情報ください');
-
-    if(isYes){
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-    }
-});
-
-function geoSuccess(position){
-    const lat = position.coords.latitude;
-
-    const lng = position.coords.longitude;
-
-    const state = `https://fukuno.jig.jp/app/printmap/latlngmap.html#15/${lat}/${lng}/&base=std&ls=std&disp=1&vs=c1j0l0u0f1`;
-    const win = window.open(`${state}`);
-    win.focus();
-}
-
-function geoError(){
-    alert('Geolocation Error');
-}
